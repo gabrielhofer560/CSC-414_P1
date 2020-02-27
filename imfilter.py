b9593b7fefc3ed6f690e22577372ee16a7232ecc
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-02-27 15:52:58 -0600

    ...

--- imfilter.py
+++ imfilter.py
@@ -1,3 +1,4 @@
+# https://en.wikipedia.org/wiki/Convolutional_neural_network
 import time
 import skimage
 from skimage import io
@@ -6,7 +7,6 @@ import matplotlib.pyplot as plt
 from skimage import img_as_float32
 import sys
 import matplotlib.image as mpimg
-
 from skimage import color
 
 indentity = np.array([
@@ -22,8 +22,26 @@ blur = np.array([
 
 sharpen = np.array([
   [0,-1,0],
-  [-1,9,-1],
+  [-1,5,-1],
   [0,-1,0]
+  ])
+
+emboss = np.array([
+  [-2,-1,0],
+  [-1,1,1],
+  [0,1,2]
+  ])
+
+left_sobel = np.array([
+  [1,0,-1],
+  [2,0,-2],
+  [1,0,-1]
+  ])
+
+edge = np.array([
+  [-1,-1,-1],
+  [-1,8,-1],
+  [-1,-1,-1]
   ])
 
 #--------------------------------------------------------------------
@@ -34,8 +52,6 @@ kernel3 = make_blur_kernel(7)
 
 #--------------------------------------------------------------------
 def rgb2gray(A):
-  print("dimensions: ")
-  print(A.shape)
   (a, b, c) = A.shape
   ret = np.zeros((a, b))
   for j in range(a):
@@ -48,52 +64,14 @@ def rgb2gray(A):
   return ret
 
 #--------------------------------------------------------------------
-def im_filter_color(A,kernel):
-
-  (mi,ni,ki) = A.shape
-  (mk,nk) = kernel.shape
-
-  Z = np.zeros((mi+mk,ni+nk,ki))
-  tmp = np.zeros((mi+mk,ni+nk,ki))
-
-  for i in range(mi):
-    for j in range(ni):
-      for k in range(ki):
-        Z[i+mk//2][j+nk//2][k]=A[i][j][k]
-
-  for p in range(ki):
-    print(str(p)+" th channel")
-    for i in range(mi):
-      print("\t"+str(i)+"/"+str(mi))
-      for j in range(ni):
-        acc=0
-        for k in range(mk):
-          for l in range(nk):
-            acc+=Z[i+k][j+l][p]*kernel[k][l]
-        tmp[i+mk//2][j+nk//2][p]=acc
-
-  for i in range(mi):
-    for j in range(ni):
-      for p in range(ki):
-        A[i][j][p]=tmp[i+mk//2][j+nk//2][p]
-
-  return A
-
-#--------------------------------------------------------------------
 def im_filter_gray(A,kernel):
-
   (mi,ni) = A.shape
   (mk,nk) = kernel.shape
-
   Z = np.zeros((mi+mk,ni+nk))
   tmp = np.zeros((mi+mk,ni+nk))
   for i in range(mi):
     for j in range(ni):
       Z[i+mk//2][j+nk//2]=A[i][j]
-
-  plt.imshow(Z)
-  plt.show()
-
   for i in range(mi):
     for j in range(ni):
       acc=0
@@ -101,40 +79,32 @@ def im_filter_gray(A,kernel):
         for l in range(nk):
           acc+=Z[i+k][j+l]*kernel[k][l]
       tmp[i+mk//2][j+nk//2]=acc
-
   for i in range(mi):
     for j in range(ni):
-      A[i][j]=tmp[i+mk//2][j+nk//2]
-
+      A[i][j]=tmp[i+mk//2][j+nk//2] 
   return A
 
 #--------------------------------------------------------------------
 def my_imfilter(filename,kernel): # ,mode,boundary):
-  A = rgb2gray(io.imread(FILE))
-  plt.imshow(A)
-  plt.show()
-
-  print("shape after rgb2gray")
-  print(A.shape)
-
+  A = io.imread(filename)
   if(len(A.shape)==2): 
     im_filter_gray(A,kernel)
   if(len(A.shape)>2): 
-    im_filter_color(A,kernel)
-
+    for i in range(A.shape[2]):
+      A[:,:,i] = im_filter_gray(A[:,:,i],kernel)
+  return A
 
 #--------------------------------------------------------------------
 def main():
   for FILE in sys.argv[1:]:
-    B = my_imfilter(FILE,sharpen)
+    B = my_imfilter(FILE,emboss)
+
+    io.imsave("out.png",B)
     plt.imshow(B)
     plt.show()
 
 main()
 
+#--------------------------------------------------------------------
 
 
-
-
-
- 
