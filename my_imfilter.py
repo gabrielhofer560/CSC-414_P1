e87c0e299ac1ae3d2c3fd41955eba3d80190a74b
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-02-26 18:07:21 -0600

    working on stuff

---
+++ my_imfilter.py
@@ -0,0 +1,125 @@
+# https://www.cc.gatech.edu/classes/AY2016/cs4476_fall/results/proj1/
+# https://www.cc.gatech.edu/classes/AY2016/cs4476_fall/results/proj1/html/ychen918/index.html
+# https://www.cc.gatech.edu/classes/AY2016/cs4476_fall/results/proj1/html/zlu83/
+# https://www.cc.gatech.edu/classes/AY2016/cs4476_fall/
+
+# http://cs.brown.edu/courses/csci1430/2013/results/proj1/
+# http://cs.brown.edu/courses/csci1430/2013/results/proj1/xl76/
+import time
+import skimage
+from skimage import io
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import img_as_float32
+import sys
+
+# https://docs.scipy.org/doc/numpy/user/basics.creation.html
+kernel0 = np.array([
+    [0,0,0],
+    [0,1,0],
+    [0,0,0]])
+
+kernel2 = np.zeros((5,5))
+kernel2[2][2]=1
+
+print("this is kernel2: ")
+print(kernel2)
+
+kernel1 = np.array([
+    [1/9,1/9,1/9],
+    [1/9,1/9,1/9],
+    [1/9,1/9,1/9]
+    ])
+
+def make_blur_kernel(dim):
+  blur = np.full((dim,dim),1/(dim*dim))
+  return blur
+
+kernel3 = make_blur_kernel(7)
+
+
+# def make_kernel(dim):
+def my_imfilter(image,kernel,filename): # ,mode,boundary):
+
+  # https://docs.scipy.org/doc/numpy/reference/generated/numpy.stack.html
+
+  A = io.imread(filename)
+   
+  print(A.shape)
+  print(len(A.shape))
+
+  print(kernel0)
+
+  print(kernel0.shape)
+  print(len(kernel0.shape))
+
+  if(len(A.shape)==2):
+    (mi,ni) = A.shape
+    (mk,nk) = kernel.shape
+  if(len(A.shape)==3):
+    (mi,ni,ki) = A.shape
+    (mk,nk,kk) = kernel.shape
+
+  plt.imshow(A)
+  plt.show()
+
+  Z = np.zeros((mi+mk,ni+nk))
+  for i in range(mi):
+    for j in range(ni):
+      Z[i+mk//2][j+nk//2]=A[i][j]
+
+  plt.imshow(Z)
+  plt.show()
+
+  for i in range(mi):
+    for j in range(ni):
+      acc=0
+      for k in range(mk):
+        for l in range(nk):
+          acc+=Z[mk//2+i][nk//2+j]*kernel[k][l]
+
+  plt.imshow(Z)
+  plt.show()
+
+for FILE in sys.argv[1:]:
+  my_imfilter(FILE,kernel3,FILE)
+
+
+
+
+
+# import time
+# import skimage
+# from skimage import io
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from skimage import img_as_float32
+# import sys
+# 
+# # start the timer
+# start = time.time()
+# 
+# def process(file):
+#   A = io.imread(file)
+#   (m1,n1) = A.shape
+#   
+#   plt.imshow(A)
+#   plt.show()
+#   
+#   # logical indexing to set values less than 10 to 0
+#   B = A < 80
+#   A[B] = 0
+#   
+#   # display the image after changes
+#   plt.imshow(A)
+#   plt.show()
+#   
+#   # stop the timer and display elapse time to modify image
+#   end = time.time()
+#   print("total time: "+str(end - start))
+# 
+# # read the image
+# for file in sys.argv[1:]:
+#   process(file)
+# 
+# 
